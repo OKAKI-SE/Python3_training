e09d46b5a80acf5d7cf4c882ae653fe5f6162c26
OKAKI_SE <reeeiapp@gmail.com>
2019-12-01 11:39:09 +0900

    update

--- ex17.py
+++ ex17.py
@@ -1,14 +1,22 @@
-dept = input("借金 >")
-nenri = input("年利率 >")
-hensai = input("返済額 >")
+#借金をして月々、定額を返済していくと借金はどうなっていくのかを調べるプログラムを作成しよう。
+# 借金の金額と、利息の年利率(%)、月々の返済額を入力すると、毎月、借金がなくなるまで月数と借金の金額を
+# 表示するものとする。月々の借金は、借金の利息年利率/12（月割り）分増加するが、返済分だけ減る。
 
-nokori = 0
+dept = int(input("借金 >"))
+nenri = float(input("年利率（％） >"))
+hensai = int(input("返済額 >"))
 total = 0
+month = 0
 
-for hensai > dept:
+while dept > hensai:
+    month += 1
+    dept = dept * (1 + nenri/12/100) - hensai
 
-    print(month,"月: 返済額 ",hensai" 円 残り ",nokori,"円")
-
-print(month,"月: 返済額 ",hensai" 円これで完済。 返済総額: ",total,"円")
-
-
+    #print(str(month)+"月: 返済額 ",hensai," 円 残り "+int(dept)+"円")
+    print(str(month)+'月: 返済額',hensai,'円','残り',int(dept),sep=' ')
+    total += hensai
+month += 1
+dept = dept * (1 + nenri/12/100)
+total += dept
+#print(str(month)+"月: 返済額 ",int(dept)," 円これで完済。 返済総額: "+int(total)+"円",sep='')
+print(str(month)+'月: 返済額',int(dept),'円','これで完済。','返済総額: ',int(total),'円',sep=' ')
